e03bece840a1b8abd07e893d36dbbf6402a75072
Taksh <dhabalia.taksh@gmail.com>
2025-01-04 23:37:34 +0530

    camera working , storage is not functional

--- Tarzan_Camera/lib/main.dart
+++ Tarzan_Camera/lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-import 'camera_page.dart';
+
+import '../pages/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Camera Feed App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: CameraPage(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
     );
   }
 }
